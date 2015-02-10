a08e93b486b93e70d4be52ce7143b4943ed87905
Yolk Huang <yolk.tw@gmail.com>
2015-02-10 16:54:50 +0800

    Initial Project

--- YHPhotoData/PhotosViewController.h
+++ YHPhotoData/PhotosViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  PhotosViewController.h
 //  YHPhotoData
 //
 //  Created by Andy Huang on 2/10/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface PhotosViewController : UIViewController
 
 @end
-
---
+++ YHPhotoData/PhotosViewController.m
@@ -0,0 +1,24 @@
+//
+//  PhotosViewController.m
+//  YHPhotoData
+//
+//  Created by Andy Huang on 2/10/15.
+//  Copyright (c) 2015 Andy Huang. All rights reserved.
+//
+
+#import "PhotosViewController.h"
+
+@implementation PhotosViewController
+
+#pragma mark - View's Lifecycle
+
+- (void)loadView {
+  [super loadView];
+  self.view.backgroundColor = [UIColor whiteColor];
+}
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+}
+
+@end
