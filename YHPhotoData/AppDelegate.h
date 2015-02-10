7f89c0c4fd470e52793e04ffad7abe66abab2e3f
Yolk Huang <yolk.tw@gmail.com>
2015-02-10 15:56:08 +0800

    Initial Commit

---
+++ YHPhotoData/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YHPhotoData
+//
+//  Created by Andy Huang on 2/10/15.
+//  Copyright (c) 2015 Andy Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YHPhotoData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YHPhotoData
+//
+//  Created by Andy Huang on 2/10/15.
+//  Copyright (c) 2015 Andy Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
